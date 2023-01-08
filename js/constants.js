8723b62ae64408e42678bfb52685e4fc7bec5fb0
Manu Sporny <msporny@digitalbazaar.com>
2023-01-08 17:22:48 -0500

    Add Verifiable Credentials v2 JSON-LD Context and library files.

---
+++ js/constants.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+module.exports = {
+  CONTEXT_FILENAME: 'credentials-v2.jsonld',
+  CONTEXT_URL: 'https://www.w3.org/ns/credentials/v2',
+  CBORLD_VALUE: 0x32
+};
